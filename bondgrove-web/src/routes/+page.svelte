7b14065071dee5c05ec3d918bcb16ce7a3e0c022
Nader Rahhal <107228500+Nader-Rahhal@users.noreply.github.com>
2024-04-07 00:37:08 -0500

    1

--- bondgrove-web/src/routes/+page.svelte
+++ bondgrove-web/src/routes/+page.svelte
@@ -1,34 +1,20 @@
 <script>
-  import { Accordion, AccordionItem } from "carbon-components-svelte";
-</script>
+    import LoginHeader from "../components/LoginHeader.svelte";
+    import { AccordionItem, Accordion } from 'flowbite-svelte';
+</script> 
 
-<svelte:head>
-  <link
-    rel="stylesheet"
-    href="https://unpkg.com/carbon-components-svelte/css/white.css"
-  />
-</svelte:head>
+<div class="index">
+  <LoginHeader />
+  <h3>From Seedling to Skyline: Grow Investments, Grow Communities</h3>
+  <p>
+    Empowering your investments to support local projects and municipal growth. Where your finances fuel the future.
+  </p>
+</div>
 
-<Accordion>
-  <AccordionItem title="Natural Language Classifier">
-    <p>
-      Natural Language Classifier uses advanced natural language processing and
-      machine learning techniques to create custom classification models. Users
-      train their data and the service predicts the appropriate category for the
-      inputted text.
-    </p>
-  </AccordionItem>
-  <AccordionItem title="Natural Language Understanding">
-    <p>
-      Analyze text to extract meta-data from content such as concepts, entities,
-      emotion, relations, sentiment and more.
-    </p>
-  </AccordionItem>
-  <AccordionItem title="Language Translator">
-    <p>
-      Translate text, documents, and websites from one language to another.
-      Create industry or region-specific translations via the service's
-      customization capability.
-    </p>
-  </AccordionItem>
-</Accordion>
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
+
+
