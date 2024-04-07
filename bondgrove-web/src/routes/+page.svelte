f872fe1a2069cf3f3a64ee353ee4fd9d08453b86
lucaskritz <lucaskritz2026@u.northwestern.edu>
2024-04-07 00:15:51 -0500

    pushed images folder

--- bondgrove-web/src/routes/+page.svelte
+++ bondgrove-web/src/routes/+page.svelte
@@ -2,7 +2,12 @@
     import LoginHeader from "../components/LoginHeader.svelte";
 
  
-</script> 
+
+  import imageSrc from 'Users\yaboy\Downloads\bondgrove_logo.webp';
+</script>
+
+<img src={imageSrc} alt="Descriptive text for accessibility">
+
 
 <div class="index">
   <LoginHeader />
@@ -14,8 +19,28 @@
 
 <style lang="postcss">
   :global(html) {
-    background-color: theme(colors.gray.100);
+    background-color: theme(colors.green.100);
+    font-size: 1.5rem;
   }
+ 
+    :global(.index h1) {
+    font-size: 2rem; /* Adjust the size as needed */
+
+  }
+  
+  :global(.signin-button) {
+    /* Tailwind utility class for margin-left auto will push the button to the right */
+    font-size: .75rem;
+    margin-left: 850px;
+  }
+
+  :global(.signup-button) {
+    /* Tailwind utility class for margin-left auto will push the button to the right */
+    font-size: .75rem;
+    margin-right: 25px;
+  }
+
+
 </style>
 
 
